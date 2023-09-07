41b02d0045e504a10a31a4d7b81acbb28fbdd3ae
DavidLeon01 <141367061+DavidLeon01@users.noreply.github.com>
2023-09-06 22:26:05 -0500

    Add files via upload

---
+++ labs/02/Menu_Matrices.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.menu_matrices;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author DavidLeon
+ */
+public class Menu_Matrices {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int opcion;
+        System.out.println("Seleccione una Opcion: \n1.Suma De Matrices \n2.Producto Punto \n3.Producto de un escalar \n4.Transpuesta de una Matriz. \n5.Que opcion desea: ");
+        opcion = entrada.nextInt();
+        
+        
+        
+    }
+}
