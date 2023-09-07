1095245347b31f7518d932cc0cfc9fee045fc98c
DavidLeon01 <141367061+DavidLeon01@users.noreply.github.com>
2023-09-06 22:52:23 -0500

    Add files via upload

--- labs/02/Menu_Matrices.java
+++ labs/02/Menu_Matrices.java
@@ -15,9 +15,31 @@ public class Menu_Matrices {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         int opcion;
+        int esp;
         System.out.println("Seleccione una Opcion: \n1.Suma De Matrices \n2.Producto Punto \n3.Producto de un escalar \n4.Transpuesta de una Matriz. \n5.Que opcion desea: ");
         opcion = entrada.nextInt();
         
+        switch (opcion){
+        case 1:{
+    System.out.println("Ingrese el numero de filas de la matriz: ");
+    esp = entrada.nextInt();
+            
+    int matrizA[][] = new int[esp][esp];
+    int matrizB[][] = new int[esp][esp];
+    int matrizC[][] = new int[esp][esp];
+    int matrizD[][] = new int[esp][esp];
+            
+    
+    for (int i=0;i<esp;i++){
+     for (int j=0; j<esp;j++){
+     System.out.println("Ingrese numero para la matriz A: ");
+     matrizA[i][j] = entrada.nextInt();
+     }
+    }
+    matrizA=
+    }
+        
+        }
         
         
     }
