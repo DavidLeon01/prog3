7a53971e2fae8128a04d997054771a064da50e97
DavidLeon01 <141367061+DavidLeon01@users.noreply.github.com>
2023-09-24 17:11:44 -0500

    Add files via upload

---
+++ labs/03/Laboratorio3.java
@@ -0,0 +1,17 @@
+
+import java.util.LinkedList;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author DavidLeon
+ */
+public class Laboratorio3 {
+
+    public static void main(String[] args) {
+        static LinkedList<Asignatura> materias = new LinkedList<>();
+    }
+}
