203ee6434536be03aa90c4507ae0cca73c261a38
DavidLeon01 <141367061+DavidLeon01@users.noreply.github.com>
2023-09-28 01:15:37 -0500

    Add files via upload

--- labs/03/02/carro.java
+++ labs/03/02/carro.java
@@ -7,6 +7,43 @@
  *
  * @author DavidLeon
  */
+ import java.text.ParseException;
+ import java.text.SimpleDateFormat;
+ import java.util.Date;
 public class carro {
-    
+   private String placa;
+   private String tipo;
+   private String IngresoP;
+   private String SalidaP;
+           
+   public carro(String placa,String tipo,String IngresoP){
+   this.placa=placa;
+   this.tipo=tipo;
+   this.IngresoP=IngresoP;
+   }
+   public String getPlaca(){
+   return placa;
+   }
+   public void setPlaca(String placa){
+   this.placa=placa;
+   }
+   public String getTipo(){
+   return tipo;
+   }
+   public void setTipo(String tipo){
+   this.tipo=tipo;
+   }
+   public String getingresoP(){
+   return IngresoP;
+   }
+   public void setingresoP(String IngresoP){
+   this.IngresoP=IngresoP;
+   }
+   public String getsalidaP(){
+   return SalidaP;
+   }
+   public void setgetsalidaP(String SalidaP){
+   this.SalidaP=SalidaP;
+       }
+   
 }
